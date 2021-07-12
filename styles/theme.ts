82df2a2c30e1dd153559bb291be7feed414c3171
Shinji Teramoto <shinji.toys@gmail.com>
2021-07-12 22:52:04 +0900

    modify mui theme breakpoints

--- styles/theme.ts
+++ styles/theme.ts
@@ -14,4 +14,13 @@ export const defaultTheme = createTheme({
       main: red.A400,
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
 })
